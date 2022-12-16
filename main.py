82c198f8d6de852737bf49b64f0cb037e14788df
MassJet <ItsHGPchannelplayz@gmail.com>
2022-12-16 17:14:32 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import datetime
+import customtkinter as ctk
 
-print(datetime.date)
+ctk.set_appearance_mode('System')
+ctk.set_default_color_theme('blue')
+
+root = ctk.CTk()
+
+root.geometry('800x800')
+root.title("EzInventory")
+
+
+if __name__ == '__main__':
+    root.mainloop()
