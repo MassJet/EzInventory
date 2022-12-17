d5521f9342ffbcdeae3ac67c5ebd264442cb63c2
MassJet <ItsHGPchannelplayz@gmail.com>
2022-12-16 19:39:19 -0500

    keyboard test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
-
+import functions
+font = 'Lato Regular'
 users = ['Select a user...','Geris', 'Henry', 'Zoe', 'Jonida', 'VERY LONG SENTENCE VERY LONG SENTENCE VERY LONG SENTENCE VERY LONG SENTENCE ']
 
 ctk.set_appearance_mode('System')
@@ -11,9 +12,12 @@ root.geometry('800x800')
 root.title("EzInventory")
 frame.pack(pady=20,padx=100, fill='both', expand=True)
 
-userselect = ctk.CTkOptionMenu(frame, values=users, width=100)
+userselect = ctk.CTkOptionMenu(frame, values=users, font=(font, 20), width=100, )
 userselect.configure(width=100)
 userselect.pack(pady=50)
+
+binum = ctk.CTkEntry(master=frame, placeholder_text='Bin Number', width=300, font=(font, 20))
+
 
 
 if __name__ == '__main__':
